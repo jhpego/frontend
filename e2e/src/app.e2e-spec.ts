1f69d78d85ed36ec371c28de746a0888ca008c98
Jorge Pego <JorgePego@compta.pt>
2020-06-28 01:28:03 +0100

    atualizacao namespaces

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('filmes app is running!');
+    expect(page.getTitleText()).toEqual('pegonet app is running!');
   });
 
   afterEach(async () => {
